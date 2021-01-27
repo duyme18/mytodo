56aa4b72f05ad139f6bed0760fd7d54c21dada9a
Duy <duyme18@gmail.com>
2021-01-28 01:17:57 +0700

    sort entity

--- springboot-server/src/main/java/com/hdd/mytodo/controller/TodoController.java
+++ springboot-server/src/main/java/com/hdd/mytodo/controller/TodoController.java
@@ -1,10 +1,12 @@
 package com.hdd.mytodo.controller;
 
+import java.util.Date;
 import java.util.List;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -16,8 +18,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hdd.mytodo.exception.ResourceNotFoundException;
 import com.hdd.mytodo.model.Todo;
+import com.hdd.mytodo.model.User;
 import com.hdd.mytodo.repository.TodoRepository;
+import com.hdd.mytodo.repository.UserRepository;
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -27,13 +32,30 @@ public class TodoController {
 	@Autowired
 	private TodoRepository todoRepository;
 
+	@Autowired
+	private UserRepository userRepository;
+
 	@GetMapping("/todos")
 	public List<Todo> getAllTodos() {
 		return todoRepository.findAll();
 	}
 
-	@PostMapping("/todos")
-	public Todo createTodo(@Valid @RequestBody Todo todo) {
+	@GetMapping(value = "/todos/user/{userId}")
+	public ResponseEntity<?> findByUser(@PathVariable Long userId) throws ResourceNotFoundException {
+		User user = userRepository.findById(userId)
+				.orElseThrow(() -> new ResourceNotFoundException("User not found for this id :: " + userId));
+		Sort sortEntity = Sort.by("createdAt");
+		List<Todo> todos = todoRepository.findByUser(user, sortEntity);
+		return ResponseEntity.ok().body(todos);
+	}
+
+	@PostMapping("/todos/{userId}")
+	public Todo createTodo(@PathVariable Long userId, @Valid @RequestBody Todo todo) throws ResourceNotFoundException {
+
+		User user = userRepository.findById(userId)
+				.orElseThrow(() -> new ResourceNotFoundException("User not found for this id :: " + userId));
+
+		todo.setUser(user);
 		todo.setCompleted(false);
 		return todoRepository.save(todo);
 	}
@@ -46,8 +68,10 @@ public class TodoController {
 
 	@PutMapping(value = "/todos/{id}")
 	public ResponseEntity<Todo> updateTodo(@PathVariable("id") Long id, @Valid @RequestBody Todo todo) {
+		Date now = new Date();
 		return todoRepository.findById(id).map(todoData -> {
 			todoData.setTitle(todo.getTitle());
+			todoData.setCreatedAt(now);
 			todoData.setCompleted(todo.getCompleted());
 			Todo updatedTodo = todoRepository.save(todoData);
 			return ResponseEntity.ok().body(updatedTodo);
--- springboot-server/src/main/java/com/hdd/mytodo/repository/TodoRepository.java
+++ springboot-server/src/main/java/com/hdd/mytodo/repository/TodoRepository.java
@@ -1,11 +1,15 @@
 package com.hdd.mytodo.repository;
 
+import java.util.List;
+
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.hdd.mytodo.model.Todo;
+import com.hdd.mytodo.model.User;
 
 @Repository
 public interface TodoRepository extends JpaRepository<Todo, Long> {
-
+	List<Todo> findByUser(User user, Sort sortEntity);
 }
