b2db2f701cebe91a3f4c5453ce953099bc798054
Duy <duyme18@gmail.com>
2021-02-02 23:34:31 +0700

    commit

--- springboot-server/src/main/java/com/hdd/mytodo/security/service/UserDetailsImpl.java
+++ springboot-server/src/main/java/com/hdd/mytodo/security/service/UserDetailsImpl.java
@@ -42,7 +42,7 @@ public class UserDetailsImpl implements UserDetails {
 		List<GrantedAuthority> authorities = user.getRoles().stream()
 				.map(role -> new SimpleGrantedAuthority(role.getName().name())).collect(Collectors.toList());
 
-		return new UserDetailsImpl(user.getId(), user.getUsername(), user.getFullName(), user.getEmail(), user.getPassword(), authorities);
+		return new UserDetailsImpl(user.getId(), user.getUsername(), user.getFullname(), user.getEmail(), user.getPassword(), authorities);
 	}
 
 	@Override
