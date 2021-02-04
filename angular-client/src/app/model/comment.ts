9939d01cd6fc8adba0bef4e5f21076762dd47d0e
Duy <duyme18@gmail.com>
2021-02-04 17:20:13 +0700

    add comment model

---
+++ angular-client/src/app/model/comment.ts
@@ -0,0 +1,10 @@
+import { User } from './user';
+import { Todo } from './todo';
+export interface Comment {
+    id: number;
+    content: string;
+    commentDate: Date;
+    isEdit: Boolean;
+    todo: Todo;
+    user: User;
+}
